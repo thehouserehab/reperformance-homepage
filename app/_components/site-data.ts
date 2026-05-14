ffa4814861c9788b357dfdc2fdde70a8891db4f0
thehouserehab <thehouserehab@the-house-exercise.com>
2026-05-14 22:53:51 +0900

    Polish header alignment and stabilize location/coach layouts

---
+++ app/_components/site-data.ts
@@ -0,0 +1,4 @@
+export const notionSurveyUrl = "https://fearless-okapi-9c9.notion.site/2cdfab3c9eaa80e8a936e85baa1f3b59?pvs=105";
+export const instagramUrl = "https://www.instagram.com/reperformance_trainer";
+export const phoneHref = "tel:01024188400";
+export const naverMapUrl = "https://naver.me/5BwnMHha";
